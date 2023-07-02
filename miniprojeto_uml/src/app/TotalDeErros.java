e9d1f044dce3423aae994f98512608d96ad9f6b9
Edyane <edyanealves@gmail.com>
2023-07-02 12:53:48 -0300

    refactor: atualizando projeto

--- miniprojeto_uml/src/app/TotalDeErros.java
+++
@@ -1,9 +0,0 @@
-package app;
-
-public class TotalDeErros extends Questoes {
-
-	public static void main(String[] args) {
-
-		System.out.println("Você errou " + erros + " de " + questoes.length + " questões.");
-	}
-}
